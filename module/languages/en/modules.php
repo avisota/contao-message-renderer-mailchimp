1aba2fded8c3700f510eb8b5e3f366d62772d117
Tristan Lins <tristan.lins@bit3.de>
2014-03-11 15:56:23 +0100

    Add module description.

---
+++ module/languages/en/modules.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Avisota newsletter and mailing system
+ * Copyright (C) 2013 Tristan Lins
+ *
+ * PHP version 5
+ *
+ * @copyright  bit3 UG 2013
+ * @author     Tristan Lins <tristan.lins@bit3.de>
+ * @package    avisota/contao-message-renderer-mailchimp
+ * @license    LGPL-3.0+
+ * @filesource
+ */
+
+
+/**
+ * Module
+ */
+$GLOBALS['TL_LANG']['MOD']['avisota-message-renderer-mailchimp'] = array(
+	'Avisota - Message renderer "MailChimp"',
+	'Message renderer for Avisota that use MailChimp templates.'
+);
